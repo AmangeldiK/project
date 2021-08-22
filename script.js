502d70b8b28089461701555eb7373f48cd9ae91c
Amangeldi <kasymbekovamangeldi10@gmail.com>
2021-08-22 18:09:44 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// const obj = {
+//     name: 'Amangeldi',
+//     age: 21,
+//     isMarried: false
+// };
+
+// console.log(obj['name'])
+
+
+
+// let arr = ['apple', 'orange', 'peach', 'banana']
+
+// console.log(arr)
+
+// const result = confirm('Are you here?')
+
+// const answer = +prompt('Are you 18', '18');
+
+// console.log(answer+10);
+
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name', '')
+// answers[1] = prompt('What is your surname', '')
+// answers[2] = prompt('How old are you', '')
+
+// console.log(typeof(answers));
+
+
+
+const category = 'toys'
+
+console.log(`https://someurl/${category}/5`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
